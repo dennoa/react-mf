383cfd517eaadcd2ffcebf89a495bb2f3e7e7d6e
Andrew Dennison <andrew.dennison@iag.com.au>
2021-08-27 13:17:55 +1000

    docker stuff

---
+++ docker-build.sh
@@ -0,0 +1,4 @@
+docker build -t dennoa/react_mf_customer_search:latest ./customer-search
+docker build -t dennoa/react_mf_property_search:latest ./property-search
+docker build -t dennoa/react_mf_vehicle_search:latest ./vehicle-search
+docker build -t dennoa/react_mf_dashboard:latest ./dashboard
